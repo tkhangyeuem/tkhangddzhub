74b7771839228ee0115f48adf6dba13e5d33f7de
tkhangnolove <149290736+tkhangyeuem@users.noreply.github.com>
2024-08-16 15:27:13 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,18 +1,18 @@
--- Yêu cầu nhập Username và Password
-print("Vui lòng nhập Username:")
+-- Yêu cầu nhập username và password
+print("Enter your Username:")
 local username = io.read()
 
-print("Vui lòng nhập Password:")
+print("Enter your Password:")
 local password = io.read()
 
--- Kiểm tra Username và Password
+-- Kiểm tra thông tin đăng nhập
 if username == "tkhangfriends" and password == "tkhangdz123" then
-    print("Đăng nhập thành công!")
-    -- Nội dung phía sau sẽ được chạy tại đây
+    print("Access Granted!")
+    -- Tiếp tục với nội dung phía sau
 else
-    print("Sai Username hoặc Password. Chương trình sẽ tự động tắt.")
-    os.exit() -- Tự động tắt chương trình
+    print("Access Denied! Exiting...")
+    os.exit()
 end
 
--- Nội dung phía sau
-print("Bạn đã vào được nội dung phía sau!")
+-- Nội dung phía sau sau khi nhập đúng thông tin
+print("Welcome to the script!")
